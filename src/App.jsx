22b74b51dc867dce033ca7dd1d41783202f67080
Xander Hill <xander.k.hill@gmail.com>
2024-12-20 16:28:18 -0600

    login and logout capabilities added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    const user = await loginService.login({
+      username, password
+    })
+
+    window.localStorage.setItem(
+      'loggedBlogAppUser', JSON.stringify(user)
+    )
+    blogService.setToken(user.token)
+    setUser(user)
+    setUsername('')
+    setPassword('')
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser(null)
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,10 +38,43 @@ const App = () => {
     )  
   }, [])
 
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+          username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+          />
+          </div>
+          <div>
+            password
+            <input
+            type="password"
+            value={password}
+            name='Password'
+            onChange={({target}) => setPassword(target.value)}
+            />
+          </div>
+          <button type ="submit">login</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <div>
+        {user.name} is logged in
+        <button type = "submit" onClick={handleLogout}>logout</button>
+      </div>
+     {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
