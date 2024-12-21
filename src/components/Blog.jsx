a309f04a636e5ec7dd0a669e4dd1ba98822133cb
Xander Hill <xander.k.hill@gmail.com>
2024-12-21 15:43:46 -0600

    css style for blogs

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,17 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+    </div>  
+  )
+}
 
 export default Blog
