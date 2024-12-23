1e90148a89bc57cc2206970228ea439cdc8608eb
Xander Hill <xander.k.hill@gmail.com>
2024-12-23 16:53:15 -0600

    test for like button

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -82,3 +82,36 @@ test('blog shows url and likes after button clicked', async () => {
     const likes = screen.queryByText('likes: 0')
     expect(likes).toBeDefined()
 })
+
+test('like button works', async () => {
+    const blog = {
+        title: 'Example blog',
+        author: 'Me',
+        url: 'www.shouldnotappear.net',
+        likes: 0,
+        user: {
+            name: 'joe'
+        }
+    }
+
+    const loggedIn = {
+        name: 'nacho'
+    }
+
+    const likeBlog = vi.fn()
+
+    const user = userEvent.setup()
+
+    render(<Blog blog={blog} loggedIn={loggedIn} likeBlog={likeBlog}/>)
+
+    const viewButton = screen.getByText('view')
+
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(likeBlog.mock.calls).toHaveLength(2)
+})
